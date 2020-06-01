582e65eed1cfdadfd250a8dae3ea44f8d8d5ea21
Cristopher <loyola.94@hotmail.com>
2020-06-01 14:46:22 -0500

    correo
    Obteniendo el correo

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+function validarCorreo(email){
+    var expReg= /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i
+    var div = document.getElementById('error'); //Obteniendo el div con el id = error
+    var correo= document.getElementById("email").value; //Obteniendo el valor del correo.
+    var ul = document.querySelector("input"); //Obteniendo input.
+    var text = document.createTextNode("Ingrese un correo Valido"); //El texto a Mostrar cuando no ingresen un correo correcto.
+
+    if(expReg.test(email)){
+        console.log("Bienvenido" + correo) //Obteniendo el correo Ingresado.
+    }else{
+        ul.style.border = "1px solid red"; // Cambiando el color del border del input.
+        div.appendChild(text);
+        
+    }
+}
